23a1180b04da18cd1e81c99012f298e853494061
XBigTK13X <xbigtk13x@gmail.com>
2012-10-14 20:33:36 -0400

    Maven added.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,4 +1,4 @@
 #! /usr/sh
 #
 
-sudo apt-get install git vim filezilla curl gitk gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-gnonlin gstreamer0.10-sdl gstreamer0.10-plugins-bad-multiverse gstreamer0.10-schroedinger gstreamer0.10-plugins-ugly totem-plugins-extra
+sudo apt-get install git vim filezilla curl gitk gstreamer0.10-ffmpeg gstreamer0.10-fluendo-mp3 gstreamer0.10-gnonlin gstreamer0.10-sdl gstreamer0.10-plugins-bad-multiverse gstreamer0.10-schroedinger gstreamer0.10-plugins-ugly totem-plugins-extra maven
