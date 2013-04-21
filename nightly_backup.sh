fdd615f320105761c18f06e579bbae608c374585
Tim Kretschmer <xbigtk13x@gmail.com>
2013-04-21 10:16:15 -0300

    Update nightly_backup.sh

--- nightly_backup.sh
+++ nightly_backup.sh
@@ -1,1 +1,2 @@
-sudo rsync -ar --progress  /media/Byte /media/Doppleganger
+sudo rsync -ar / /media/kretst/BUP/Linux --exclude /mnt --exclude /dev --exclude /proc --exclude /media
+sudo rsync -ar /media/Byte /media/kretst/BUP/Windows
